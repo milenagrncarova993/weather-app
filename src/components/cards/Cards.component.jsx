72789f62393ffefd7fb5ab0a59824876ae2e56ea
Milena Grncarova <milenagrncarov@outlook.com>
2021-03-09 13:06:15 +0100

    Working on style of the cards

--- src/components/cards/Cards.component.jsx
+++ src/components/cards/Cards.component.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import './Cards.component.css'
 
 const CardItem = ({day,temp,windDirection,windSpeed,type}) =>(
 
     <div className='content'>
+        <img src="./images/weather.png.jpg" alt="Sample photo"></img>
         <h1 className='day'>{day.toUpperCase()}</h1>
-        <h1 className='temp'>{temp}</h1>
+        <p className='temp'>{temp}</p>
     </div>
 )
 
 
 export default CardItem;
+
--- src/components/days/Days.component.jsx
+++ src/components/days/Days.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CardItem from '../cards/Cards.component';
+import './Days.component.css';
 
 
 class Days extends React.Component{
