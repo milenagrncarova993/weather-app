4b34abc1e5f80f180933aab781a632aefdcb0b4e
Milena Grncarova <milenagrncarov@outlook.com>
2021-03-09 11:50:19 +0100

    Mapping over the days

---
+++ src/components/days/Days.component.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+
+
+class Days extends React.Component{
+    constructor(){
+        super();
+
+        this.state = {
+            days: [
+                {
+                    day:'Monday',
+                    temp:22,
+                    windDirection:'north-east',
+                    windSpeed:10,
+                    type:'sunny'
+                },
+                {
+                    day:'Tuesday',
+                    temp:14,
+                    windDirection:'north-west',
+                    windSpeed:14,
+                    type:'rainy'
+                },
+                {
+                    day:'Wednesday',
+                    temp:17,
+                    windDirection:'south-east',
+                    windSpeed:20,
+                    type:'cloudy'
+                },
+                {
+                    day:'Thursday',
+                    temp:17,
+                    windDirection:'south-east',
+                    windSpeed:20,
+                    type:'cloudy'
+                },
+                {
+                    day:'Friday',
+                    temp:14,
+                    windDirection:'north-west',
+                    windSpeed:14,
+                    type:'rainy'
+                },
+                {
+                    day:'Saturday',
+                    temp:22,
+                    windDirection:'north-east',
+                    windSpeed:10,
+                    type:'sunny'
+                },
+                {
+                    day:'Sunday',
+                    temp:22,
+                    windDirection:'north-east',
+                    windSpeed:10,
+                    type:'sunny'
+                }
+            ]
+        }
+    }
+
+    render(){
+        return(
+            <div className='days-menu'>
+                {
+                    this.state.days.map(({day,temp,windDirection,windSpeed,type}))
+                }
+            </div>
+
+        );
+    }
+}
+
+export default Days;
