5d5d491eda1cde620ef10fe5fb4bffec2c6dc1d5
Milena Grncarova <milenagrncarov@outlook.com>
2021-03-09 12:38:47 +0100

    Created a styles sheet and showing only the day and temp

--- src/components/cards/Cards.component.jsx
+++ src/components/cards/Cards.component.jsx
@@ -4,10 +4,7 @@ const CardItem = ({day,temp,windDirection,windSpeed,type}) =>(
 
     <div className='content'>
         <h1 className='day'>{day.toUpperCase()}</h1>
-        <h1 className='day'>{temp}</h1>
-        <h1 className='day'>{windDirection}</h1>
-        <h1 className='day'>{windSpeed}</h1>
-        <h1 className='day'>{type}</h1>
+        <h1 className='temp'>{temp}</h1>
     </div>
 )
 
